244423ae5df3ed9c75b29960b0477ff272591ba1
lucaswatanuki <lucaswatanuki@gmail.com>
2018-04-09 09:16:24 -0300

    Test Commit

---
+++ Teste.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int main(){
+
+int a, b, c;
+float x, y, i;
+
+x=1;
+y=0;
+i=0;
+
+a=1;
+b=2;
+c=10;
+
+a=b=c;
+x=y=i;
+
+
+cout << a << "\n";
+cout << b << "\n";
+cout << c << "\n";
+cout << x << "\n";
+cout << y << "\n";
+cout << i << "\n";
+
+return 0;
+}
